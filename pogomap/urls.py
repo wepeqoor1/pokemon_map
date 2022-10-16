17f2fd47f352b07fbe222c5ad17b580bb065c985
wepeqoor1 <hoopeeeee@gmail.com>
2022-10-16 13:02:40 +0300

    Delete django-debyg-toolbar

--- pogomap/urls.py
+++ pogomap/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from pokemon_entities import views
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_all_pokemons, name='mainpage'),
     path('pokemon/<pokemon_id>/', views.show_pokemon, name='pokemon'),
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 
